ea05f784f91d1bd12d4f65e246dd6b6ac2bc221f
rishavraj19595 <rishavraj19595@gmail.com>
2024-10-27 05:14:00 +0530

    Update scripts.js

--- hacka/js/scripts.js
+++ hacka/js/scripts.js
@@ -12,5 +12,5 @@ function showSlides() {
     slideIndex++;
     if (slideIndex > slides.length) {slideIndex = 1}    
     slides[slideIndex - 1].style.display = "block";  
-    setTimeout(showSlides, 4000); // Change image every 4 seconds
+    setTimeout(showSlides, 2500);
 }
